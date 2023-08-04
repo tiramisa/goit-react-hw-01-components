bb669a131e7957232931aa7e9724439e90e7685e
tiramisa <miller19988@gmail.com>
2023-08-05 00:29:33 +0100

    fix friend table

--- src/components/Transaction/TransactionHistory.jsx
+++ src/components/Transaction/TransactionHistory.jsx
@@ -1,33 +1,7 @@
 import React from 'react';
+import { tableStyles, thStyles, tdStyles, transactionAllStyles } from './TransactionHistoryStyles';
 
 const TransactionHistory = ({ items }) => {
-  const tableStyles = {
-    width: '500px',
-    borderCollapse: 'collapse',
-    margin: '0',
-    fontFamily: 'Arial, sans-serif',
-    textAlign: 'center',
-  };
-
-  const thStyles = {
-    color: '#333',
-    padding: '12px',
-    fontSize: '16px',
-    backgroundColor: '#96e0dd',
-  };
-
-  const tdStyles = {
-    border: '1px solid #ddd',
-    padding: '12px',
-    fontSize: '14px',
-    backgroundColor: '#96a0e0',
-  };
-
-  const transactionAllStyles = {
-    margin: '100px',
-    boxShadow: '0 9px 10px rgba(0, 0, 0, 0.2), 9px 0 10px rgba(0, 0, 0, 0.2)',
-  };
-
   return (
     <div style={transactionAllStyles}>
       <table style={tableStyles}>
@@ -53,3 +27,4 @@ const TransactionHistory = ({ items }) => {
 };
 
 export default TransactionHistory;
+
